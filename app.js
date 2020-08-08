e50ca8fef85a38a0d75e54693655e8ae24b1ee12
Jon Wright <dr.wrong.mo@gmail.com>
2020-08-08 17:04:12 -0500

    Clearing Productin Errors

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_public')));
 
