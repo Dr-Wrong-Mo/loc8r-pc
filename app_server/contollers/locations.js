1e2233a482b8fff7c153a36c28e6122191ff6f01
Jon Wright <dr.wrong.mo@gmail.com>
2020-08-08 14:02:22 -0500

    heroku test

--- app_server/contollers/locations.js
+++ app_server/contollers/locations.js
@@ -5,6 +5,7 @@ const apiOptions = {
 if (process.env.NODE_ENV === 'production') {
   apiOptions.server = 'https://enigmatic-badlands-07570.herokuapp.com/';
 }
+const apiKey = DB_apiKey;
 
 const showError = (req, res, status) =>  {
   let title = '';
