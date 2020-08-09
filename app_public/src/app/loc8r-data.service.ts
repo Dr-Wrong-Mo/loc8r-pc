27835e1888cb662074e713cc56860eff2fa749d6
Jon Wright <dr.wrong.mo@gmail.com>
2020-08-09 16:06:33 -0500

    test change to data service

--- app_public/src/app/loc8r-data.service.ts
+++ app_public/src/app/loc8r-data.service.ts
@@ -9,7 +9,7 @@ export class Loc8rDataService {
 
   constructor(private http: HttpClient) { }
 
-  private apiBaseUrl = 'http://localhost:3000/api';
+  private apiBaseUrl = 'https://enigmatic-badlands-07570.herokuapp.com'
 
   public getLocations(): Promise<Location[]> {
     const lng: number = -94.576320;
