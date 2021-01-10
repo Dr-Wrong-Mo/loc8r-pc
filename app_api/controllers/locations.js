449459ea8ffde63aad121798cd737c49212fde8f
Jon Wright <dr.wrong.mo@gmail.com>
2021-01-10 17:33:22 -0600

    API correction. Location Update One fixed

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -5,199 +5,161 @@ const locationsListByDistance = async (req, res) => {
   const lng = parseFloat(req.query.lng);
   const lat = parseFloat(req.query.lat);
   const near = {
-    type: "Point",
-    coordinates: [lng, lat]
+    type: 'Point',
+    coordinates: [lng, lat],
   };
   const geoOptions = {
-    distanceField: "distance.calculated",
+    distanceField: 'distance.calculated',
     key: 'coords',
     spherical: true,
-    maxDistance: 20000
+    maxDistance: 20000,
     /*limit: 10*/
   };
   if (!lng || !lat) {
     return res
       .status(404)
-      .json({ "message": "lng and lat query parameters are required" });
+      .json({ message: 'lng and lat query parameters are required' });
   }
   try {
     const results = await Loc.aggregate([
       {
         $geoNear: {
           near,
-          ...geoOptions
-        }
-      }
+          ...geoOptions,
+        },
+      },
     ]);
-    const locations = results.map(result => {
+    const locations = results.map((result) => {
       return {
         _id: result._id,
         name: result.name,
         address: result.address,
         rating: result.rating,
         facilities: result.facilities,
-        distance: `${result.distance.calculated.toFixed()}`
-      }
+        distance: `${result.distance.calculated.toFixed()}`,
+      };
     });
-    res
-      .status(200)
-      .json(locations);
+    res.status(200).json(locations);
   } catch (err) {
-    res
-      .status(404)
-      .json(err);
+    res.status(404).json(err);
   }
 };
-  
+
 const locationsCreate = (req, res) => {
-    Loc.create({
+  Loc.create(
+    {
       name: req.body.name,
       address: req.body.address,
-      facilities: req.body.facilities.split(","),
+      facilities: req.body.facilities.split(','),
       coords: {
-        type: "Point",
-        coordinates: [
-          parseFloat(req.body.lng),
-          parseFloat(req.body.lat)
-         ]
+        type: 'Point',
+        coordinates: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
       },
       openingTimes: [
         {
           days: req.body.days1,
           opening: req.body.opening1,
           closing: req.body.closing1,
-          closed: req.body.closed1
+          closed: req.body.closed1,
         },
         {
           days: req.body.days2,
           opening: req.body.opening2,
           closing: req.body.closing2,
-          closed: req.body.closed2
-        }
-      ]
+          closed: req.body.closed2,
+        },
+      ],
     },
     (err, location) => {
       if (err) {
-        res
-          .status(400)
-          .json(err);
+        res.status(400).json(err);
       } else {
-        res
-          .status(201)
-          .json(location);
+        res.status(201).json(location);
       }
-    });
-  };
+    }
+  );
+};
 
 const locationsReadOne = (req, res) => {
-    Loc
-        .findById(req.params.locationid)
-        .exec((err, location) => {
-            if (!location) {
-                return res
-                    .status(404)
-                    .json({ "message": "location not found" });
-            } else if (err) {
-                return res
-                    .status(404)
-                    .json(err);
-            } else {
-                return res
-                    .status(200)
-                    .json(location);
-            }
-        });
+  Loc.findById(req.params.locationid).exec((err, location) => {
+    if (!location) {
+      return res.status(404).json({ message: 'location not found' });
+    } else if (err) {
+      return res.status(404).json(err);
+    } else {
+      return res.status(200).json(location);
+    }
+  });
 };
 
 const locationsUpdateOne = (req, res) => {
-    if (!req.params.locationid) {
-      return res
-        .status(404)
-        .json({
-          "message": "Not found, locationid is required"
+  if (!req.params.locationid) {
+    return res.status(404).json({
+      message: 'Not found, locationid is required',
+    });
+  }
+  Loc.findById(req.params.locationid)
+    .select('-reviews -rating')
+    .exec((err, location) => {
+      if (!location) {
+        return res.status(404).json({
+          message: 'locationid not found',
         });
-    }
-    Loc
-      .findById(req.params.locationid)
-      .select('-reviews -rating')
-      .exec((err, location) => {
-        if (!location) {
-          return res
-            .status(404)
-            .json({
-              "message": "locationid not found"
-            });
-        } else if (err) {
-          return res
-            .status(400)
-            .json(err);
-        }
-        location.name = req.body.name;
-        location.address = req.body.address;
-        location.facilities = req.body.facilities.split(',');
-  
-        location.coords = [
-          parseFloat(req.body.lng),
-          parseFloat(req.body.lat)
-        ];
-        location.openingTimes = [{
+      } else if (err) {
+        return res.status(400).json(err);
+      }
+      location.name = req.body.name;
+      location.address = req.body.address;
+      location.facilities = req.body.facilities.split(',');
+
+      location.coords = {
+        type: 'Point',
+        coordinates: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+      };
+      location.openingTimes = [
+        {
           days: req.body.days1,
           opening: req.body.opening1,
           closing: req.body.closing1,
           closed: req.body.closed1,
-        }, {
+        },
+        {
           days: req.body.days2,
           opening: req.body.opening2,
           closing: req.body.closing2,
           closed: req.body.closed2,
-        }];
-        location.save((err, loc) => {
-          if (err) {
-            res
-              .status(404)
-              .json(err);
-          } else {
-            res
-              .status(200)
-              .json(loc);
-          }
-        });
-      }
-    );
-  };
+        },
+      ];
+      location.save((err, loc) => {
+        if (err) {
+          res.status(404).json(err);
+        } else {
+          res.status(200).json(loc);
+        }
+      });
+    });
+};
 
 const locationsDeleteOne = (req, res) => {
-const {locationid} = req.params;
-if (locationid) {
-    Loc
-    .findByIdAndRemove(locationid)
-    .exec((err, location) => {
-        if (err) {
-            return res
-            .status(404)
-            .json(err);
-        }
-        res
-            .status(204)
-            .json(null);
-        }
-    );
-} else {
-    res
-    .status(404)
-    .json({
-        "message": "No Location"
+  const { locationid } = req.params;
+  if (locationid) {
+    Loc.findByIdAndRemove(locationid).exec((err, location) => {
+      if (err) {
+        return res.status(404).json(err);
+      }
+      res.status(204).json(null);
     });
-}
+  } else {
+    res.status(404).json({
+      message: 'No Location',
+    });
+  }
 };
-  
 
 module.exports = {
-    locationsListByDistance,
-    locationsCreate,
-    locationsReadOne,
-    locationsUpdateOne,
-    locationsDeleteOne
+  locationsListByDistance,
+  locationsCreate,
+  locationsReadOne,
+  locationsUpdateOne,
+  locationsDeleteOne,
 };
-
-
